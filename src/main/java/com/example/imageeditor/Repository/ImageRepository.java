f38db767caf8033f7a4f64c82c3132681933f84b
Dima <gordundima@gmail.com>
2024-10-14 14:56:51 +0300

    Додав код

---
+++ src/main/java/com/example/imageeditor/Repository/ImageRepository.java
@@ -0,0 +1,10 @@
+package com.example.imageeditor.Repository;
+
+import com.example.imageeditor.model.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImageRepository extends JpaRepository<Image, Long> {
+
+}
